41d61c1103ca913be9d01316e34ee693be0676f6
jojo313 <32206115+jojo313@users.noreply.github.com>
2019-12-12 18:00:20 +0100

    Update IR_test.ino
    Serial output: 1 for nothing
    0 for object

--- IR_test/IR_test.ino
+++ IR_test/IR_test.ino
@@ -1,12 +1,16 @@
 //Pin: G2
 
+int val = 5;
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(2, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  val = digitalRead(2);
+  Serial.println(val);
+  delay(500);
 }
